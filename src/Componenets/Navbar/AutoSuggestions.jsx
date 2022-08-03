7b3b750fb4a3bef1d9609b7dfa3b3d1a8b59d21b
Nitesh Kumar Goshwami <86410121+Nitesh-Goshwami@users.noreply.github.com>
2022-08-03 09:46:41 +0530

    changed in auto suggestion

--- src/Componenets/Navbar/AutoSuggestions.jsx
+++ src/Componenets/Navbar/AutoSuggestions.jsx
@@ -88,6 +88,22 @@ export default function AutoSuggestion(props) {
 
 // Top films as rated by IMDb users. http://www.imdb.com/chart/top
 const topFilms = [
+    {
+        title: "Star Wars: Episode IV - A New Hope",
+        year: 1977,
+    },
+    { title: "City of God", year: 2002 },
+    { title: "Se7en", year: 1995 },
+    { title: "The Silence of the Lambs", year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: "Life Is Beautiful", year: 1997 },
+    { title: "The Usual Suspects", year: 1995 },
+    { title: "Léon: The Professional", year: 1994 },
+    { title: "Spirited Away", year: 2001 },
+    { title: "Saving Private Ryan", year: 1998 },
+    { title: "Once Upon a Time in the West", year: 1968 },
+    { title: "American History X", year: 1998 },
+    { title: "Interstellar", year: 2014 },
     { title: "Aquatic Plants", year: 1994 },
     { title: "Bulbs", year: 1972 },
     { title: "Bamboo", year: 1974 },
@@ -115,20 +131,5 @@ const topFilms = [
     { title: "Silver Oak Plant", year: 1990 },
     { title: "Hibiscus Pink Plant", year: 1999 },
     { title: "Seven Samurai", year: 1954 },
-    {
-        title: "Star Wars: Episode IV - A New Hope",
-        year: 1977,
-    },
-    { title: "City of God", year: 2002 },
-    { title: "Se7en", year: 1995 },
-    { title: "The Silence of the Lambs", year: 1991 },
-    { title: "It's a Wonderful Life", year: 1946 },
-    { title: "Life Is Beautiful", year: 1997 },
-    { title: "The Usual Suspects", year: 1995 },
-    { title: "Léon: The Professional", year: 1994 },
-    { title: "Spirited Away", year: 2001 },
-    { title: "Saving Private Ryan", year: 1998 },
-    { title: "Once Upon a Time in the West", year: 1968 },
-    { title: "American History X", year: 1998 },
-    { title: "Interstellar", year: 2014 },
+
 ];
